4f88d7491e091ae77d091d8df724d6a6a2645909
Aleksandr Pak <aleksandr.pak@undev.ru>
2015-08-29 14:16:05 +0300

    added 1.4

---
+++ books/cracking_the_code_interview_5th_edition/chapter_1/question_4_replace_spaces/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+void replace(char* str) {
+	int i;
+	int len = strlen(str);
+	int spaceCount = 0;
+
+	for (i = 0; i < len; ++i) {
+		if (str[i] == ' ') {
+			++spaceCount;
+		}
+	}
+
+	int newLen = len + spaceCount * 2;
+	str[newLen] = 0;
+
+	for (i = 0; i < len; ++i) {
+		if (str[len - i - 1] == ' ') {
+			str[--newLen - i] = '0';
+			str[--newLen - i] = '2';
+			str[newLen - i - 1] = '%';
+		} else {
+			str[newLen - i - 1] = str[len - i - 1];
+		}
+	}
+}
+
+int main() {
+	char str[18];
+	strcpy(str, "Mr John Smith");
+
+	printf("before replacing: %s\n", str);
+
+	replace(str);
+
+	printf("after replacing: %s", str);	
+}
