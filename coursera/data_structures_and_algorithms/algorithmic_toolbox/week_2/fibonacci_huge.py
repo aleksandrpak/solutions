e09524eb0131ce6275cdf489aa0d1b8b27cab840
Aleksandr Pak <alekspak@icloud.com>
2016-04-05 21:33:44 +0200

    implemented huge fib algorithm

--- coursera/data_structures_and_algorithms/algorithmic_toolbox/week_2/fibonacci_huge.py
+++ coursera/data_structures_and_algorithms/algorithmic_toolbox/week_2/fibonacci_huge.py
@@ -1,11 +1,21 @@
 # Uses python3
-import sys
+def fib_huge(n, m):
+    if (m == 1):
+        return 0
 
-def get_fibonaccihuge(n, m):
-    # write your code here
-    return 0
+    ns = [0, 1, 1]
+    f = (1, 1)
+    p = set([])
 
-if __name__ == '__main__':
-    input = sys.stdin.read();
-    n, m = map(int, input.split())
-    print(get_fibonaccihuge(n, m))
+    while (f not in p):
+        next = (f[0] + f[1]) % m
+        ns.append(next)
+        p.add(f)
+        f = (f[1], next)
+
+    r = n % len(p)
+
+    return ns[r]
+
+n, m = map(int, input().split())
+print(fib_huge(n, m))
