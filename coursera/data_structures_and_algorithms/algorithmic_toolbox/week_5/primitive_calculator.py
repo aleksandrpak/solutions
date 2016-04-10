95d85d6aaba06314fc19f9b7aae9034ce0a87776
Aleksandr Pak <alekspak@icloud.com>
2016-04-10 21:03:10 +0200

    almost working calculator

--- coursera/data_structures_and_algorithms/algorithmic_toolbox/week_5/primitive_calculator.py
+++ coursera/data_structures_and_algorithms/algorithmic_toolbox/week_5/primitive_calculator.py
@@ -2,16 +2,29 @@
 import sys
 
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
+    s = {1:(1, [1])}
+
+    for i in range(2, n + 1):
+        l1 = s[i - 1][0]
+        l2, l3 = 0, 0
+
+        if (i % 2 == 0):
+            l2 = s[i / 2][0]
+        if (i % 3 == 0):
+            l3 = s[i / 3][0]
+
+        l, seq = 0, []
+        if (l2 != 0 and l2 <= l1 and (l3 == 0 or l2 <= l3)):
+            l, seq = s[i / 2][0], list(s[i / 2][1])
+        elif (l3 != 0 and l3 <= l1 and (l2 == 0 or l3 <= l2)):
+            l, seq = s[i / 3][0], list(s[i / 3][1])
         else:
-            n = n - 1
-    return reversed(sequence)
+            l, seq = s[i - 1][0], list(s[i - 1][1])
+
+        seq.append(i)
+        s[i] = (l + 1, seq)
+
+    return s[n][1]
 
 input = sys.stdin.read()
 n = int(input)
